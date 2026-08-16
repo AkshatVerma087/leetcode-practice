4ceab05fefe6c56e5497b987d03ecf7364a92387
Akshat Verma <144297460+AkshatVerma087@users.noreply.github.com>
2026-08-17 01:58:43 +0530

    Time: 7 ms (29.14%) | Memory: 114.1 MB (100.00%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+       int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+       for(int stone : stones) {
+        if(stone % 3 == 0) {
+            cnt0++;
+        }else if(stone % 3 == 1){ 
+            cnt1++;
+        }else{
+            cnt2++;
+        }
+       } 
+
+       if(cnt0 % 2 == 0) {
+        return (cnt1 >= 1 && cnt2 >= 1) && (cnt2 >= cnt1 || cnt1 >= cnt2);
+       }
+
+       return Math.abs(cnt2 - cnt1)  >=3;
+    }
+}
